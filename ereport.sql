a3de2cffbbec4553228ddd0c8d65618529adb1dd
nurchulis <nura3609@gmail.com>
2019-03-22 15:28:30 +0700

    update database

--- ereport.sql
+++ ereport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 20, 2019 at 04:32 PM
+-- Generation Time: Mar 22, 2019 at 03:28 PM
 -- Server version: 5.7.21-1
 -- PHP Version: 7.0.29-1+b1
 
@@ -68,10 +68,22 @@ INSERT INTO `Gambar` (`id_gambar`, `related_id`, `name_gambar`, `create_date`) V
 
 CREATE TABLE `Join_task` (
   `id_join` int(5) NOT NULL,
-  `id_task` int(5) NOT NULL,
+  `id_task` int(10) NOT NULL,
   `id_user` int(5) NOT NULL,
   `roles` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `Join_task`
+--
+
+INSERT INTO `Join_task` (`id_join`, `id_task`, `id_user`, `roles`) VALUES
+(1, 1, 1, 'boss'),
+(2, 1, 2, 'staff'),
+(3, 2, 3, 'staff'),
+(4, 2, 4, 'boss'),
+(5, 3, 1, 'staff'),
+(35, 3, 2, 'staff');
 
 -- --------------------------------------------------------
 
@@ -80,11 +92,23 @@ CREATE TABLE `Join_task` (
 --
 
 CREATE TABLE `Task` (
-  `id_task` int(7) NOT NULL,
+  `id_task` int(10) NOT NULL,
   `description` varchar(200) NOT NULL,
-  `directory` varchar(50) NOT NULL,
   `name_location` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32;
+
+--
+-- Dumping data for table `Task`
+--
+
+INSERT INTO `Task` (`id_task`, `description`, `name_location`) VALUES
+(1, 'Foto Pengerjaan Renovasi Jalan Parangtritis KM 10.5', 'bantul Ysogyakarta'),
+(2, 'Laporan Foto Pembangunan Jembatan Bantul', 'Jetis Bantul Yogyakarta'),
+(3, 'Laporan Foto Pembangunan Irigasi', 'Pundong Bantul Yogyakarta'),
+(417865, 'Foto Pengerjaan Renovasi Jalan Parangtritis KM 10.5', 'bantul Yogyakarta'),
+(475598, 'Foto Pengerjaan Renovasi Jalan Parangtritis KM 10.5', 'bantul Yogyakarta'),
+(971541, 'Foto Pengerjaan Renovasi Jalan Parangtritis KM 10.5', 'bantul Yogyakarta'),
+(988514, 'Foto Pengerjaan Renovasi Jalan Parangtritis KM 10.5', 'bantul Yogyakarta');
 
 -- --------------------------------------------------------
 
@@ -104,13 +128,16 @@ CREATE TABLE `User` (
 --
 
 INSERT INTO `User` (`id_user`, `username`, `email`, `password`) VALUES
-(1, 'oeosaajosaskasaojssjoas', '', '$pbkdf2-sha256$29000$IqRUylnrHaP0Pqc0ZkzpnQ$7CapBSYfidSPXtjDMLnkMY3f7YGy27ZS38keHQVHxMg'),
+(1, 'linaa', '', '$pbkdf2-sha256$29000$IqRUylnrHaP0Pqc0ZkzpnQ$7CapBSYfidSPXtjDMLnkMY3f7YGy27ZS38keHQVHxMg'),
 (2, 'nurchulis', '', '$pbkdf2-sha256$29000$EoLwntN6z3mvdc659x7jnA$ixGHe4UKMF/lG3TOR4toU9fiJ9hSxhpjnpVjh22kLBE'),
 (3, 'ahsiahsaishiash', '', '$pbkdf2-sha256$29000$t7a2trZWylnLOUdIac35nw$aV9YFXuM1lW6NQKuYOjZwcSO/I5HLjmpmrecQikH6bE'),
 (4, 'chulis', '', '$pbkdf2-sha256$29000$4Nw7x7i39v7/f89Zy7kXog$uz0MhHnM2zlS7J4j6/gaDaNegjPn2ND3sFASCeHGdAs'),
 (5, 'aku', '', '$pbkdf2-sha256$29000$7l3rndOaU.odg/Aew/gfAw$xHm6ckXRVCv2.64CXSQuf/u.J14/9AcySCdaX2qa1e4'),
 (6, 'latifahlina', 'email.com', '$pbkdf2-sha256$29000$CYEQovQeQyjlXGtNqZXyXg$07igEDYM4rzxdxWLIxFxZtNLVJi53jXAqP.IgXi0Uis'),
-(7, 'lina', 'lina.com', '$pbkdf2-sha256$29000$GwOgFIJwDqFUSsk5R8gZww$Zux2DU52zLGMkxp0IA9jUfK.IoG7TzrTlt1DJWWnQ0M');
+(7, 'lina', 'lina.com', '$pbkdf2-sha256$29000$GwOgFIJwDqFUSsk5R8gZww$Zux2DU52zLGMkxp0IA9jUfK.IoG7TzrTlt1DJWWnQ0M'),
+(8, 'malikea', 'lina.dcom', '$pbkdf2-sha256$29000$mTOGMGbMmROCUMoZ45yzFg$dHfurg0zE66ReqgsdoIh8FyE6LRv7/1wCDg445Q5Vsw'),
+(9, 'malikssea', 'lssina.dcom', '$pbkdf2-sha256$29000$CkGIsZYyRojxfk/J2dsbww$Fr2wqQFPKmc6xocKQiaN2kvUKG5Goy8rv8.Vbb0j/zM'),
+(10, 'maschulis', 'asas.dcom', '$pbkdf2-sha256$29000$ihEixFjLec.5N8Y459xbyw$bynMqg5Pj9FD2L4/UKWU8YbZSOwBNDEzJHcAnuhoEIM');
 
 --
 -- Indexes for dumped tables
@@ -165,17 +192,17 @@ ALTER TABLE `Gambar`
 -- AUTO_INCREMENT for table `Join_task`
 --
 ALTER TABLE `Join_task`
-  MODIFY `id_join` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_join` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `Task`
 --
 ALTER TABLE `Task`
-  MODIFY `id_task` int(7) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_task` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=988515;
 --
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
